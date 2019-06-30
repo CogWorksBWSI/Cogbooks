edfa3b0b1acae1a8c866fe63fa85fd2d9c2b7df6
Ryan Soklaski <ry26099@mit.edu>
2019-06-30 17:02:51 -0400

    formatting

--- tests/test_file_handling.py
+++ tests/test_file_handling.py
@@ -26,32 +26,44 @@ def test_file_ipynb_doesnt_exist():
 
 @pytest.mark.usefixtures("cleandir")
 @settings(deadline=None, max_examples=10)
-@given(num_files=st.sampled_from(range(4)), already_exists=st.lists(st.sampled_from(range(4)), unique=True))
+@given(
+    num_files=st.sampled_from(range(4)),
+    already_exists=st.lists(st.sampled_from(range(4)), unique=True),
+)
 def test_multiple_files_in_dir_where_some_exist(num_files: int, already_exists: list):
     Path("./dummy").mkdir(exist_ok=True)
     for i in range(num_files):
-        shutil.copy('test_files/test.md', f'dummy/test{i}.md')
+        shutil.copy("test_files/test.md", f"dummy/test{i}.md")
 
     for i in already_exists:
-        Path(f'dummy/test{i}_STUDENT.ipynb').touch()
+        Path(f"dummy/test{i}_STUDENT.ipynb").touch()
 
     os.system(f"cogbooks dummy")
-    assert all([Path(f'dummy/test{i}_STUDENT.ipynb').exists() for i in range(num_files)])
+    assert all(
+        [Path(f"dummy/test{i}_STUDENT.ipynb").exists() for i in range(num_files)]
+    )
     shutil.rmtree(Path("./dummy"))
 
 
 @pytest.mark.usefixtures("cleandir")
 @settings(deadline=None, max_examples=10)
-@given(num_files=st.sampled_from(range(4)), already_exists=st.lists(st.sampled_from(range(4)), unique=True))
+@given(
+    num_files=st.sampled_from(range(4)),
+    already_exists=st.lists(st.sampled_from(range(4)), unique=True),
+)
 def test_multiple_files_where_some_exist(num_files: int, already_exists: list):
     Path("./dummy").mkdir(exist_ok=True)
     for i in range(num_files):
-        shutil.copy('test_files/test.md', f'dummy/test{i}.md')
+        shutil.copy("test_files/test.md", f"dummy/test{i}.md")
 
     for i in already_exists:
-        Path(f'dummy/test{i}_STUDENT.ipynb').touch()
+        Path(f"dummy/test{i}_STUDENT.ipynb").touch()
 
-    list_of_names = ' '.join([f'dummy/test{i}.md' for i in set(list(range(num_files)) + already_exists)])
+    list_of_names = " ".join(
+        [f"dummy/test{i}.md" for i in set(list(range(num_files)) + already_exists)]
+    )
     os.system(f"cogbooks {list_of_names}")
-    assert all([Path(f'dummy/test{i}_STUDENT.ipynb').exists() for i in range(num_files)])
+    assert all(
+        [Path(f"dummy/test{i}_STUDENT.ipynb").exists() for i in range(num_files)]
+    )
     shutil.rmtree(Path("./dummy"))
